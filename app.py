c04b96a4f8c32011b856d2f5ec2c92172d355ee1
henokali1 <henokali1@gmail.com>
2026-01-06 09:39:39 +0400

    port changed

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def service_worker():
     return send_from_directory('static', 'sw.js')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=9996)
